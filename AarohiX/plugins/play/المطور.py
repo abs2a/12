4bad069c8ce77a9ca4e08f0a3e2439f4e29a55ac
abs2a <131925760+abs2a@users.noreply.github.com>
2024-03-31 20:59:39 +0300

    Update المطور.py

--- AarohiX/plugins/play/المطور.py
+++ AarohiX/plugins/play/المطور.py
@@ -5,7 +5,7 @@ from AarohiX import app
 import config
 
 @app.on_message(
-    command(["اوامر", "الاوامر"])
+    command(["ميوزك", "اااههزو"])
 )
 async def mmmezat(client, message):
     await message.reply_text(
